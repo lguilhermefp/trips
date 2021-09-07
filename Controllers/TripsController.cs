8c7eab6cac0f0bcf97172f4fbc3f1b8fcccd9162
luis guilherme <luis-guilherme.pereira@unesp.br>
2021-09-06 23:47:14 -0300

    adding post trip and get trips endpoints

---
+++ Controllers/TripsController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using Trips.Data;
+
+namespace Trips.Controllers
+{
+	[Route("api/[controller]")]
+	public class TripsController: Controller
+	{
+		private ITripService _service;
+		public TripsController(ITripService service)
+		{
+			this._service = service;
+		}
+
+		[HttpGet("[Action]")]
+		public IActionResult GetTrips()
+		{
+			var allTrips = _service.GetAllTrips();
+			return Ok(allTrips);
+		}
+
+		[HttpPost("[Action]")]
+		public IActionResult AddTrip([FromBody]Trip trip)
+		{
+			if(trip != null)
+			{
+				_service.AddTrip(trip);
+			}
+			return Ok();
+		}
+	}
+}
--- Data/Services/TripService.cs
+++ Data/Services/TripService.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Trips.Data
 {
@@ -6,7 +7,7 @@ namespace Trips.Data
 	{
 		public void AddTrip(Trip trip)
 		{
-			throw new System.NotImplementedException();
+			Data.Trips.Add(trip);
 		}
 
 		public void DeleteTrip(int tripId)
@@ -14,11 +15,8 @@ namespace Trips.Data
 			throw new System.NotImplementedException();
 		}
 
-		public List<Trip> GetAllTrips()
-		{
-			throw new System.NotImplementedException();
-		}
-
+		public List<Trip> GetAllTrips() => Data.Trips.ToList();
+		
 		public Trip GetTripById(int tripId)
 		{
 			throw new System.NotImplementedException();
